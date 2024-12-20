9be254cbf4153a9795045fdd634ca31ded8c0dcb
Toni Haryanto <toha.samba@gmail.com>
2024-12-20 15:29:13 +0700

    Remove unused

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,6 +6,3 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-
-// $routes->resource('dashboard/statistic', ['controller' => '\App\Pages\Dashboard\Statistic\PageController']);
-// $routes->resource('dashboard', ['controller' => '\App\Pages\Dashboard\PageController']);
